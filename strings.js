ce6bae7f33ce2c36b45ea0525420126c859cc138
Nathan Ostrander <ostrander_nathan@yahoo.com>
2016-09-01 12:06:53 -0500

    add index / js

---
+++ strings.js
@@ -0,0 +1,39 @@
+var testString = "";
+var btn = document.getElementById("btn");
+btn.addEventListener('click', createString);
+
+var input = "";
+
+var results = document.getElementById("results");
+
+function reversal(value) {
+  return value.split("").reverse().join("");
+};
+
+function alphabits(value) {
+  return value.split("").sort().join("")
+};
+
+function palindrome(value) {
+  if (value === reversal(value)) {
+    return "It is a palindrome"
+  } else {
+    return "Palindrome"
+  }
+};
+
+function createString() {
+  testString = document.getElementById("inputText").value
+  results.innerHTML = "<div>" + "Word" + testString + "</div>";
+  results.innerHTML += "<div>This is your alphabet " + alphabits(testString) + "</div>";
+  results.innerHTML += "<div>This is your reverse " + reversal(testString) + "</div>";
+  results.innerHTML += "<div>Not a Sarah " + palindrome(testString) + "</div>";
+};
+
+function validate(event) {
+  if (event.keycode == 13 || event.which == 13) {
+    results();
+  }
+};
+
+inputText.addEventListener('keydown', validate);
