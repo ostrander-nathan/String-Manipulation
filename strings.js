c61acdb716ff148942b91dbacdf891814ef9a91f
Nathan Ostrander <ostrander_nathan@yahoo.com>
2017-04-26 13:59:23 -0500

    clean & readme

--- strings.js
+++ strings.js
@@ -6,34 +6,39 @@ var input = "";
 
 var results = document.getElementById("results");
 
+function lettersOnly(event) {
+    var key = event.keyCode;
+    return ((key > 64 && key < 91) || (key > 96 && key < 123) || key == 8);
+};
+
 function reversal(value) {
-  return value.split("").reverse().join("");
+    return value.split("").reverse().join("");
 };
 
 function alphabits(value) {
-  return value.split("").sort().join("")
+    return value.split("").sort().join("")
 };
 
 function palindrome(value) {
-  if (value === reversal(value)) {
-    return "It is a palindrome"
-  } else {
-    return "Palindrome"
-  }
+    if (value === reversal(value)) {
+        return "It is a palindrome"
+    } else {
+        return "Palindrome"
+    }
 };
 
 function createString() {
-  testString = document.getElementById("inputText").value
-  results.innerHTML = "<div>" + "Word" + testString + "</div>";
-  results.innerHTML += "<div>This is your alphabet " + alphabits(testString) + "</div>";
-  results.innerHTML += "<div>This is your reverse " + reversal(testString) + "</div>";
-  results.innerHTML += "<div>Not a Sarah " + palindrome(testString) + "</div>";
+    testString = document.getElementById("inputText").value
+    results.innerHTML = "<div>" + "Word" + testString + "</div>";
+    results.innerHTML += "<div>This is your alphabet " + alphabits(testString) + "</div>";
+    results.innerHTML += "<div>This is your reverse " + reversal(testString) + "</div>";
+    results.innerHTML += "<div>Not a Sarah " + palindrome(testString) + "</div>";
 };
 
 function validate(event) {
-  if (event.keycode == 13 || event.which == 13) {
-    results();
-  }
+    if (event.keycode == 13 || event.which == 13) {
+        results();
+    }
 };
 
 inputText.addEventListener('keydown', validate);
